a47555b9ce5b9c1ed298b8ab36e15723609c7a06
Alen <len.gevorgyan@gmail.com>
2022-08-10 00:12:42 +0400

    Configured build script

---
+++ dist/index.js
@@ -0,0 +1,48 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const database_service_1 = require("./src/services/database.service");
+const accounts_router_1 = require("./src/routes/accounts.router");
+const dotenv = __importStar(require("dotenv"));
+// Pull the data from .env file
+dotenv.config();
+const port = process.env.PORT || 3000;
+const app = (0, express_1.default)();
+(0, database_service_1.connectToDatabase)()
+    .then(() => {
+    // Listen for accounts requests from the main page
+    app.use("/", accounts_router_1.accountsRouter);
+    app.listen(port, () => {
+        console.log(`Server started at http://localhost:${port}`);
+    });
+})
+    .catch((error) => {
+    console.error("Database connection failed", error);
+    process.exit(1);
+});
